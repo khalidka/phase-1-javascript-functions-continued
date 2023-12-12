5b42504141ec94733abaa583c8f9c6137816791e
root <root@DESKTOP-3S5SIJL.localdomain>
2023-12-12 19:19:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction());
+console.log(encouragingPromptFunction("awesome"));
